fcf3b38115304a2ba05542ee2f7cc9dce2fbe153
senthilr <senthilr@thoughtworks.com>
2017-04-01 12:54:35 +0530

    fixed deployables path issue

--- build_scripts/openerp/functional_test.sh
+++ build_scripts/openerp/functional_test.sh
@@ -5,5 +5,5 @@ printf '%s\n' "${PWD}"
 echo "The current working directory $PWD."
 sudo cp -R ./openerp-modules/* $OPENERP_LOCATION/openerp/addons/
 #Working directory openerp-functional-tests
-./openerp-functional-tests/run.sh || true
-zip -r deployables/openerp-modules.zip openerp-modules --exclude=*.git*
+cd openerp-functional-tests && ./run.sh || true
+zip -r ../deployables/openerp-modules.zip ../openerp-modules --exclude=*.git* --exclude=*.git*
