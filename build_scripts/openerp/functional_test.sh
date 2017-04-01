e41965079224f16d932a4c06f852a4d2c11914fa
senthilr <senthilr@thoughtworks.com>
2017-04-01 12:18:35 +0530

    changed funtionaltest.sh to print work directory

--- build_scripts/openerp/functional_test.sh
+++ build_scripts/openerp/functional_test.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
 rm -rf deployables
 mkdir -p deployables
+printf '%s\n' "${PWD}"
+echo "The current working directory $PWD."
 sudo cp -R ./openerp-modules/* $OPENERP_LOCATION/openerp/addons/
 #Working directory openerp-functional-tests
 ./openerp-functional-tests/run.sh || true
