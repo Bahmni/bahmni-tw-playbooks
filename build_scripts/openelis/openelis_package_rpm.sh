86e1a1a1aa8d6a292418f02185adece37f52d90a
senthilr <senthilr@thoughtworks.com>
2017-04-03 21:32:24 +0530

    fixed openelis revison path

--- build_scripts/openelis/openelis_package_rpm.sh
+++ build_scripts/openelis/openelis_package_rpm.sh
@@ -4,7 +4,7 @@ set -e
 #Fetch Artifact	Passed	Pipeline Name: OpenElis_#{pipeline_name} Stage Name: Build Job Name: build Source File: deployables/openelis.war Destination: bahmni_package/bahmni-lab/resources
 #Fetch Artifact	Passed	Pipeline Name: OpenElis_#{pipeline_name} Stage Name: Build Job Name: build Source File: deployables/OpenElis.zip Destination: bahmni_package/bahmni-lab/resources
 #Working Directory: bahmni_package
-./bahmni-lab/test/bahmni_openelis_revision.sh > bahmni-lab/test/bahmni_openelis_revision.json
+./bahmni_package/bahmni-lab/test/bahmni_openelis_revision.sh > /bahmni_package/bahmni-lab/test/bahmni_openelis_revision.json
 gunzip emr-functional-tests/dbdump/openelis_backup.sql.gz
 mv emr-functional-tests/dbdump/openelis_backup.sql bahmni_package/bahmni-lab/resources/openelis_demo_dump.sql
 #working directort: bahmni_package
