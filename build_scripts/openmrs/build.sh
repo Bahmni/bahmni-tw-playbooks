c688caa3d81593701a3eb4a7d73a4ad5fec020a1
senthilr <senthilr@thoughtworks.com>
2017-04-04 11:41:37 +0530

    updated openmrs build script

--- build_scripts/openmrs/build.sh
+++ build_scripts/openmrs/build.sh
@@ -2,4 +2,4 @@
 gunzip emr-functional-tests/dbdump/openmrs_backup.sql.gz
 mv emr-functional-tests/dbdump/openmrs_backup.sql bahmni-package/openmrs/resources/openmrs_demo_dump.sql
 #Working directory: bahmni-package
-./gradlew -PbahmniRelease=#{rpm_version} clean --debug :openmrs:buildRpm
+cd bahmni-package && ./gradlew -PbahmniRelease=${rpm_version} clean --debug :openmrs:buildRpm
