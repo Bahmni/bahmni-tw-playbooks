1e1ed2eee92ec762e086decb3c9f032be1767849
senthilr <senthilr@thoughtworks.com>
2017-04-04 21:48:48 +0530

    removed env option from gauge command on functional test deploy build script

--- build_scripts/functional_test/deploy.sh
+++ build_scripts/functional_test/deploy.sh
@@ -12,11 +12,11 @@ mkdir -p deployables/downloaded_rpms/rpms
 cd deployables/downloaded_rpms/rpms && sudo yum install -y bahmni-openmrs*.rpm bahmni-web*.rpm bahmni-emr*.rpm && cd ../../..
 sudo service openmrs start
 echo BAHMNI_GAUGE_APP_URL = https://$HOSTNAME.mybahmni.org > /var/lib/go-agent/pipelines/$GO_PIPELINE_NAME/emr-functional-tests/spec-results/hostname.txt
-#sleep 2m
+sleep 2m
 sudo service httpd restart
 sudo service openmrs restart
 #Working directory: bahmni-gauge
 cd bahmni-gauge && mvn clean install
 printf '%s\n' "${PWD}"
 echo "The current working directory $PWD."
-cd ${specs} &&  mvn gauge:execute env=ci -Dtags=${bahmni_gauge_tags} -DinParallel=true -Dnodes=${parallel_threads}
+cd ${specs} &&  mvn gauge:execute -Dtags=${bahmni_gauge_tags} -DinParallel=true -Dnodes=${parallel_threads}
