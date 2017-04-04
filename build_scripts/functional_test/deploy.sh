183745820b5bb86acb8a40f17bc9259ece05f2ea
senthilr <senthilr@thoughtworks.com>
2017-04-04 17:43:54 +0530

    fixed bahmni gauge default path issue on functiontest build script

--- build_scripts/functional_test/deploy.sh
+++ build_scripts/functional_test/deploy.sh
@@ -16,5 +16,5 @@ sleep 2m
 sudo service httpd restart
 sudo service openmrs restart
 #Working directory: bahmni-gauge
-cd bahmni-gauge && mvn clean install && cd ..
+cd bahmni-gauge && mvn clean install
 cd ${specs} &&  mvn gauge:execute --env ci -Dtags=${bahmni_gauge_tags} -DinParallel=true -Dnodes=${parallel_threads}
