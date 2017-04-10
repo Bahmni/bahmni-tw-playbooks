ade7ef5579836a4b2d74f896a24f5ef51c1b5f3a
senthilr <senthilr@thoughtworks.com>
2017-04-10 17:49:09 +0530

    updated cleanup artifact script

--- build_scripts/Cleanup_Artifacts/schedule_cleanup.sh
+++ build_scripts/Cleanup_Artifacts/schedule_cleanup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-find ${artifact_location}-type f -mtime +7 -name bahmni-installer-${rpm_version}-* -execdir ls -la -- {} \;
+find ${artifact_location} -type f -mtime +7 -name bahmni-installer-${rpm_version}-* -execdir ls -la -- {} \;
