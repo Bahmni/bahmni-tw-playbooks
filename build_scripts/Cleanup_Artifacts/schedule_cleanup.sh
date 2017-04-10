d6004a84ee05453257792e5716e0a6575a50c7fd
senthilr <senthilr@thoughtworks.com>
2017-04-10 20:30:18 +0530

    updated clenaup artifacts

--- build_scripts/Cleanup_Artifacts/schedule_cleanup.sh
+++ build_scripts/Cleanup_Artifacts/schedule_cleanup.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 set -e
 find ${artifact_location} -type f -mtime +7 -name bahmni-installer-${rpm_version}-* -execdir ls -la -- {} \;
+find ${artifact_location} -type f -mtime +7 -name bahmni-emr-${rpm_version}-* -execdir ls -la -- {} \;
+find ${artifact_location} -type f -mtime +7 -name bahmni-erp-${rpm_version}-* -execdir ls -la -- {} \;
+find ${artifact_location} -type f -mtime +7 -name bahmni-erp-connect-${rpm_version}-* -execdir ls -la -- {} \;
+find ${artifact_location} -type f -mtime +7 -name bahmni-event-log-service-${rpm_version}-* -execdir ls -la -- {} \;
+find ${artifact_location} -type f -mtime +7 -name bahmni-implementer-interface-${rpm_version}-* -execdir ls -la -- {} \;
+find ${artifact_location} -type f -mtime +7 -name bahmni-lab-${rpm_version}-* -execdir ls -la -- {} \;
+find ${artifact_location} -type f -mtime +7 -name bahmni-lab-connect-${rpm_version}-* -execdir ls -la -- {} \;
+find ${artifact_location} -type f -mtime +7 -name bahmni-offline-${rpm_version}-* -execdir ls -la -- {} \;
+find ${artifact_location} -type f -mtime +7 -name bahmni-openmrs-${rpm_version}-* -execdir ls -la -- {} \;
+find ${artifact_location} -type f -mtime +7 -name bahmni-reports-${rpm_version}-* -execdir ls -la -- {} \;
+find ${artifact_location} -type f -mtime +7 -name dcm4chee-${rpm_version}-* -execdir ls -la -- {} \;
+find ${artifact_location} -type f -mtime +7 -name bahmni-web-${rpm_version}-* -execdir ls -la -- {} \;
+find ${artifact_location} -type f -mtime +7 -name pacs-integration-${rpm_version}-* -execdir ls -la -- {} \;
