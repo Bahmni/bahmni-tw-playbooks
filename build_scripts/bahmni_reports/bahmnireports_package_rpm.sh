e5f59b271ba8738f06144f50ea8b89c90b95d942
senthilr <r.senthilreddy@gmail.com>
2017-06-02 22:54:18 +0530

    Update bahmnireports_package_rpm.sh

--- build_scripts/bahmni_reports/bahmnireports_package_rpm.sh
+++ build_scripts/bahmni_reports/bahmnireports_package_rpm.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 set -e
-#Fetch Artifact	Passed	Pipeline Name: Bahmni_Reports_$#{pipeline_name} Stage Name: BuildStage Job Name: Build-Bahmni-Reports Source File: deployables/bahmnireports.war Destination: bahmni_package/bahmni-reports/resources
-#Working directory: bahmni_package
 cd bahmni_package && ./gradlew -PbahmniRelease=${rpm_version} :bahmni-reports:clean :bahmni-reports:buildRpm
