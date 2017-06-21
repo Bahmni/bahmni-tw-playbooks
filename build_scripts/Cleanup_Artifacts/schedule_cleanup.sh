2f9b7be7a68efccd17b4691b33deceeffa314b37
senthilr <r.senthilreddy@gmail.com>
2017-06-21 12:14:41 +0530

    moved 7 days to 30 days to keep old artifacts

--- build_scripts/Cleanup_Artifacts/schedule_cleanup.sh
+++ build_scripts/Cleanup_Artifacts/schedule_cleanup.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 set -e
-find ${artifact_location} -type f -mtime +7 -name bahmni-installer-${rpm_version}-* -execdir rm -rf -- {} \;
-find ${artifact_location} -type f -mtime +7 -name bahmni-emr-${rpm_version}-* -execdir rm -rf -- {} \;
-find ${artifact_location} -type f -mtime +7 -name bahmni-erp-${rpm_version}-* -execdir rm -rf -- {} \;
-find ${artifact_location} -type f -mtime +7 -name bahmni-erp-connect-${rpm_version}-* -execdir rm -rf -- {} \;
-find ${artifact_location} -type f -mtime +7 -name bahmni-event-log-service-${rpm_version}-* -execdir rm -rf -- {} \;
-find ${artifact_location} -type f -mtime +7 -name bahmni-implementer-interface-${rpm_version}-* -execdir rm -rf -- {} \;
-find ${artifact_location} -type f -mtime +7 -name bahmni-lab-${rpm_version}-* -execdir rm -rf -- {} \;
-find ${artifact_location} -type f -mtime +7 -name bahmni-lab-connect-${rpm_version}-* -execdir rm -rf -- {} \;
-find ${artifact_location} -type f -mtime +7 -name bahmni-offline-${rpm_version}-* -execdir rm -rf -- {} \;
-find ${artifact_location} -type f -mtime +7 -name bahmni-openmrs-${rpm_version}-* -execdir rm -rf -- {} \;
-find ${artifact_location} -type f -mtime +7 -name bahmni-reports-${rpm_version}-* -execdir rm -rf -- {} \;
-find ${artifact_location} -type f -mtime +7 -name dcm4chee-${rpm_version}-* -execdir rm -rf -- {} \;
-find ${artifact_location} -type f -mtime +7 -name bahmni-web-${rpm_version}-* -execdir rm -rf -- {} \;
-find ${artifact_location} -type f -mtime +7 -name pacs-integration-${rpm_version}-* -execdir rm -rf -- {} \;
+find ${artifact_location} -type f -mtime +30 -name bahmni-installer-${rpm_version}-* -execdir rm -rf -- {} \;
+find ${artifact_location} -type f -mtime +30 -name bahmni-emr-${rpm_version}-* -execdir rm -rf -- {} \;
+find ${artifact_location} -type f -mtime +30 -name bahmni-erp-${rpm_version}-* -execdir rm -rf -- {} \;
+find ${artifact_location} -type f -mtime +30 -name bahmni-erp-connect-${rpm_version}-* -execdir rm -rf -- {} \;
+find ${artifact_location} -type f -mtime +30 -name bahmni-event-log-service-${rpm_version}-* -execdir rm -rf -- {} \;
+find ${artifact_location} -type f -mtime +30 -name bahmni-implementer-interface-${rpm_version}-* -execdir rm -rf -- {} \;
+find ${artifact_location} -type f -mtime +30 -name bahmni-lab-${rpm_version}-* -execdir rm -rf -- {} \;
+find ${artifact_location} -type f -mtime +30 -name bahmni-lab-connect-${rpm_version}-* -execdir rm -rf -- {} \;
+find ${artifact_location} -type f -mtime +30 -name bahmni-offline-${rpm_version}-* -execdir rm -rf -- {} \;
+find ${artifact_location} -type f -mtime +30 -name bahmni-openmrs-${rpm_version}-* -execdir rm -rf -- {} \;
+find ${artifact_location} -type f -mtime +30 -name bahmni-reports-${rpm_version}-* -execdir rm -rf -- {} \;
+find ${artifact_location} -type f -mtime +30 -name dcm4chee-${rpm_version}-* -execdir rm -rf -- {} \;
+find ${artifact_location} -type f -mtime +30 -name bahmni-web-${rpm_version}-* -execdir rm -rf -- {} \;
+find ${artifact_location} -type f -mtime +30 -name pacs-integration-${rpm_version}-* -execdir rm -rf -- {} \;
 sudo createrepo --verbose --update --cachedir /tmp/rpmcache/ --workers 6 /var/www/rpm/bahmni/
 aws s3 sync --only-show-errors --delete /var/www/rpm/bahmni/ s3://repo.mybahmni.org/rpm/bahmni/
