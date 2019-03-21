51af55c9d52dc2018401b420190fe6cacf428baf
Rajashri <jadhavrajashri49@gmail.com>
2019-03-21 17:53:56 +0530

    Update integration tests file

--- build_scripts/bahmni_connect/integration_test.sh
+++ build_scripts/bahmni_connect/integration_test.sh
@@ -2,9 +2,13 @@
 set -e
 sleep 1m
 
+cd bahmni-android/android && cordova platform remove android
+cordova platform add android
+git checkout .
+cd platforms/android
+gradle clean test
+
 #Working directory: bahmni-android/android/platforms/android/
-cd bahmni-android/android/platforms/android/
-./gradlew clean test
 #Connect to geny motion instance
 # adb connect 172.21.3.153:5555
 # ./gradlew clean connectedAndroidTest --debug
