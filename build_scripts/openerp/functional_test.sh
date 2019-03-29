3ef3c587c837460e4cadc93dc9b5a2e784f79c9f
Himabindu T <tbindu@thoughtworks.com>
2019-03-29 16:09:25 +0530

    Bindu | BAH-749 | changed openerp-modules to odoo-modules for building odoo rpm (#2)

--- build_scripts/openerp/functional_test.sh
+++ build_scripts/openerp/functional_test.sh
@@ -4,7 +4,7 @@ rm -rf deployables
 mkdir -p deployables
 printf '%s\n' "${PWD}"
 echo "The current working directory $PWD."
-sudo cp -R ./openerp-modules/* $OPENERP_LOCATION/openerp/addons/
+sudo cp -R ./odoo-modules/* $ODOO_LOCATION/odoo/addons/
 #Working directory openerp-functional-tests
 cd openerp-functional-tests && ./run.sh || true
-zip -r ../deployables/openerp-modules.zip ../openerp-modules --exclude=*.git* --exclude=*.git*
+zip -r ../deployables/odoo-modules.zip ../odoo-modules --exclude=*.git* --exclude=*.git*
