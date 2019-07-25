98a6b3ae34ab55c6c04cc1f0be079273c591541a
angshu <angshuonline@gmail.com>
2019-07-25 22:29:31 +0530

    BAH-652 | Fixing file copy error on ci build agent. fixed copy path

--- build_scripts/bahmni_mrs/bahmni_mrs_copy_default_db.sh
+++ build_scripts/bahmni_mrs/bahmni_mrs_copy_default_db.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 gunzip emr-functional-tests/dbdump/openmrs_backup.sql.gz
-mv emr-functional-tests/dbdump/openmrs_backup.sql bahmni-package/openmrs/resources/openmrs_demo_dump.sql
+mv emr-functional-tests/dbdump/openmrs_backup.sql bahmni-package/bahmni-emr/resources/openmrs_demo_dump.sql
