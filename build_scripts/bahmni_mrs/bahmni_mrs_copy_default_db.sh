53e2c270f05bbcd7f7636bb366fbc10b0808c079
angshu <angshuonline@gmail.com>
2019-07-26 13:49:36 +0530

    BAH-652 | fixing typo, the checkout directory is named bahmni_package as against bahmni-package git repo

--- build_scripts/bahmni_mrs/bahmni_mrs_copy_default_db.sh
+++ build_scripts/bahmni_mrs/bahmni_mrs_copy_default_db.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -e
 gunzip emr-functional-tests/dbdump/openmrs_backup.sql.gz
-mv emr-functional-tests/dbdump/openmrs_backup.sql bahmni-package/bahmni-emr/resources/openmrs_demo_dump.sql
+mv emr-functional-tests/dbdump/openmrs_backup.sql bahmni_package/bahmni-emr/resources/openmrs_demo_dump.sql
+
+
