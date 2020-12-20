ab42b74ac712efe55fa7944bf9d46376b33f32bd
Angshuman Sarkar <angshuonline@gmail.com>
2020-12-20 16:25:04 +0530

    Changing variables to be passed through env, instead of arguments

--- build_scripts/bahmni_connect/fetch_artifacts.sh
+++ build_scripts/bahmni_connect/fetch_artifacts.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 set -e
-github_token=$1
-connect_android_artifact_id=$2
-connect_pwa_artifact_id=$3
-rpm_version=$4
-go_pipeline_counter=$5
+v_github_token=${GITHUB_TOKEN}
+v_connect_android_artifact_id=${connect_android_artifact_id}
+v_connect_pwa_artifact_id=${connect_pwa_artifact_id}
+v_rpm_version=${rpm_version}
+v_go_pipeline_counter=${GO_PIPELINE_COUNTER}
 
-echo "Trying to get artifacts for connect-pwa:${connect_pwa_artifact_id} connect-android:${connect_android_artifact_id} for rpm-version: ${rpm_version} pipeline-counter: ${go_pipeline_counter}"
+echo "Trying to get artifacts for connect-pwa:${v_connect_pwa_artifact_id} connect-android:${v_connect_android_artifact_id} for rpm-version: ${v_rpm_version} pipeline-counter: ${v_go_pipeline_counter}"
 
 echo "Downloading Connect android apk zip ..... "
-wget --header "Authorization: token ${github_token}" --output-document=bahmni-connect-apk.zip https://api.github.com/repos/Bahmni/bahmni-offline/actions/artifacts/${connect_android_artifact_id}/zip
+wget --header "Authorization: token ${v_github_token}" --output-document=bahmni-connect-apk.zip https://api.github.com/repos/Bahmni/bahmni-offline/actions/artifacts/${v_connect_android_artifact_id}/zip
 echo "Downloading Connect pwa zip ..... "
-wget --header "Authorization: token ${github_token}" --output-document=bahmni-connect-apps.zip https://api.github.com/repos/Bahmni/bahmni-connect/actions/artifacts/${connect_pwa_artifact_id}/zip
+wget --header "Authorization: token ${v_github_token}" --output-document=bahmni-connect-apps.zip https://api.github.com/repos/Bahmni/bahmni-connect/actions/artifacts/${v_connect_pwa_artifact_id}/zip
 
 echo "Checking if downloaded apk zip is expandable ... "
 unzip -o bahmni-connect-apk.zip
-mv app-debug.apk bahmni-connect-android-${rpm_version}-${go_pipeline_counter}.apk
+mv app-debug.apk bahmni-connect-android-${v_rpm_version}-${v_go_pipeline_counter}.apk
 
 echo "Checking if downloaded pwa zip is expandable ... "
 rm -rf bahmni-connect-apps
