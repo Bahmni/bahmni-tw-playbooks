0ce767b326a308879df2dd6c1b81f0d3f2211823
MOHANKUMAR T <mohan13081999@gmail.com>
2021-09-01 10:33:12 +0530

    BDI-4|Mohan|Add|Commands to extract migrations zip in docker build script

--- build_scripts/openelis/docker_build.sh
+++ build_scripts/openelis/docker_build.sh
@@ -7,7 +7,13 @@ cd ..
 #Fetching Database Backup Data
 #gunzip emr-functional-tests/dbdump/openelis_backup.sql.gz
 #mv emr-functional-tests/dbdump/openelis_backup.sql bahmni_package/bahmni-lab/resources/openelis_demo_dump.sql
-#Building Docker images
 cd bahmni-package/bahmni-lab
+#Extracting Migrations Zip
+if [ ! -d build/migrations ]
+then
+mkdir -p build/migrations
+fi
+unzip -u -d build/migrations resources/OpenElis.zip
+#Building Docker images
 docker build -t bahmni/openelis-db:latest -f docker/db.Dockerfile . --no-cache
 docker build -t bahmni/openelis:latest -f docker/Dockerfile . --no-cache
